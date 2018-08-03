5669be679ba928bfaa1e71084185c82bd2100cdd
Hannah Wang <hannahwang12@gmail.com>
2018-08-02 22:06:56 -0400

    add business projects tab

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ $(document).ready(function() {
   });
 });
 
+function openProjectsTab(e, tabName) {
+  var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    e.currentTarget.className += " active";
+}
+
 function openAVModal() {
 	document.getElementById('AVModal').style.display = "block";
 }
