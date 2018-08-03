6bada11ced25854e9e6f1dc4f29d8534ff3b62fc
Hannah Wang <hannahwang12@gmail.com>
2018-08-02 22:17:03 -0400

    fix tab display on load

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ $(document).ready(function() {
 function openProjectsTab(e, tabName) {
   var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
     tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].className = tabcontent[i].className.replace(" active", "");
     }
-    document.getElementById(tabName).style.display = "block";
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).className += " active";
     e.currentTarget.className += " active";
 }
 
